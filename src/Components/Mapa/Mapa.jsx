d8784eb1e1806d75e41489d10cca55da32854b0f
JBahamondesRamirez <jbahamondesramirez@gmail.com>
2023-10-27 14:01:04 -0300

    button for geolocation is added

--- src/Components/Mapa/Mapa.jsx
+++ src/Components/Mapa/Mapa.jsx
@@ -1,42 +1,41 @@
 import { React, useState } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import './Mapa.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faLocationCrosshairs} from '@fortawesome/free-solid-svg-icons'
 
 export default function Mapa() {
-  const [latitude, setLatitude] = useState(-34.97806252027577);
-  const [longitude, setLongitude] = useState(-71.25259385438525);
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  const Props = {
-    zoom: 15,
-    center: {
-      lat: latitude,
-      lng: longitude
-    }
-  };
-
-  const { isLoaded } = useLoadScript({ googleMapsApiKey: `${import.meta.env.VITE_APP_API_KEY}` });
-  if (isLoaded) return <Map latitude={Props.center.lat} longitude={Props.center.lng} />
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: `${import.meta.env.VITE_APP_API_KEY}`});
+  if (isLoaded) return <Map/>
 }
 
-function Map({ latitude, longitude }) {
+
+function Map() {
+  const [latitude, setLatitude] = useState(-34.97806252027577);
+  const [longitude, setLongitude] = useState(-71.25259385438525);
+  
+  const getposition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude)
+          setLongitude(position.coords.longitude)
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
+
   return <GoogleMap 
-  zoom={15} 
+  zoom={16} 
   center={{ lat: latitude, lng: longitude }} 
   mapContainerClassName="container-map"
   options={{
-    fullscreenControl:false
-    
-  }}>
+    fullscreenControl:false,
+    streetViewControl:false,
+    mapTypeControl:false
+  }}><button className="button-geolocation" onClick={getposition}><FontAwesomeIcon icon={faLocationCrosshairs}/></button>
   </GoogleMap>
 }
